45e89575ce6bcfc7798c4057ac1ecfcc1d8d55e0
Faith0918 <sinsil0918@gmail.com>
2022-04-24 03:41:21 +0900

    fixed bug

--- kss/week4/hw/boj23289.py
+++ kss/week4/hw/boj23289.py
@@ -48,11 +48,11 @@ dc = [1,-1,0,0]
 def chill_edge():
     global t_board,r,c
     # 위 (0,[0-c])
-    for i in range(c):
+    for i in range(1,c-1):
         if t_board[0][i]>0:
             t_board[0][i]-=1
     # 아래 (r-1,[0-c])
-    for i in range(c):
+    for i in range(1,c-1):
         if t_board[r-1][i]>0:
             t_board[r-1][i]-=1
     # 왼 ([0-r],0)
@@ -63,10 +63,10 @@ def chill_edge():
     for i in range(r):
         if t_board[i][c-1]>0:
             t_board[i][c-1]-=1
-    t_board[0][0]+=1
-    t_board[0][c-1]+=1
-    t_board[r-1][0]+=1
-    t_board[r-1][c-1]+=1
+    # t_board[0][0]+=1
+    # t_board[0][c-1]+=1
+    # t_board[r-1][0]+=1
+    # t_board[r-1][c-1]+=1
     
 def is_done():
     global t_board, check_locs,k
@@ -86,53 +86,30 @@ def blow_heater():# bfs로 풀어야함.
     change_dir = {0:[2,3],1:[2,3],2:[1,0],3:[1,0]}
     d_board = [[0 for _ in range(c)] for _ in range(r)]
     for heater in heaters:
+        # pdb.set_trace()
         rr,cc,d = heater
         visited = [[False for _ in range(c)] for _ in range(r)]
         q = deque()
         nr = rr+dr[d]
         nc = cc+dc[d]
         q.append([nr,nc,d,5,0])
-        print("heater",heater)
-        print("before heater")
-        print_board(d_board)
         while q:
-            # pdb.set_trace()
             cr,cc,cd, cn,is_edge = q.popleft()
             d_board[cr][cc] += cn
             nr = cr+dr[cd]
             nc = cc+dc[cd]
             nn = cn-1
-            print(cr,cc,cd,cn)
-            if cn == 3 and cc == 3:
-                pdb.set_trace()
-            # if cn == 5:
-            #     if 0<=nr<r and 0<=nc<c and visited[nr][nc]==False and not is_blocked(cr,cc,cd):
-            #         q.append([nr,nc,cd,nn,0])
-            #         visited[nr][nc]=True
-            #     for ni, nd in enumerate(change_dir[cd]):
-            #         nnr = nr+dr[nd]
-            #         nnc = nc+dc[nd]
-            #         if 0<=nnr<r and 0<=nnc<c and visited[nnr][nnc]==False and not is_blocked(cr,cc,nd):
-            #             q.append([nnr,nnc,cd,nn,ni+1])
-            #             visited[nnr][nnc]=True
-            # else:
+            
             if nn>0:
                 if 0<=nr<r and 0<=nc<c and visited[nr][nc]==False and not is_blocked(cr,cc,cd):
                     q.append([nr,nc,cd,nn,0])
                     visited[nr][nc]=True
-                # if is_edge>0:
-                    # nd = change_dir[cd][is_edge-1]
-                    for nd in change_dir[cd]:
-                        nnr = nr+dr[nd]
-                        nnc = nc+dc[nd]
-                        # pdb.set_trace()
-                        # if nnr == 3 and nnc == 3:
-                        #     pdb.set_trace()
-                        if 0<=nnr<r and 0<=nnc<c and visited[nnr][nnc]==False and not (is_blocked(cr,cc,nd) and is_blocked(nnr-dr[cd],nnc-dc[cd],cd)):
-                            q.append([nnr,nnc,cd,nn,is_edge])
-                            visited[nnr][nnc]=True
-        print("after heater")
-        print_board(d_board)
+                for nd in change_dir[cd]:
+                    nnr = nr+dr[nd]
+                    nnc = nc+dc[nd]
+                    if 0<=nnr<r and 0<=nnc<c and visited[nnr][nnc]==False and not (is_blocked(cr,cc,nd) or is_blocked(nnr-dr[cd],nnc-dc[cd],cd)):
+                        q.append([nnr,nnc,cd,nn,is_edge])
+                        visited[nnr][nnc]=True
 
     return d_board
 
@@ -164,26 +141,18 @@ def change_board(b):
 
 while True:
     d_board = blow_heater()
-    print_board(d_board)
     change_board(d_board)
     d_board = defuse_heat()
-    print("defuse diff board")
-    print_board(d_board)
-    print("before defusing")
-    print_board(t_board)
     change_board(d_board)
-    print("defuse t_board")
-    print_board(t_board)
     chill_edge()
-    print("after chill edge")
-    print_board(t_board)
     t+=1
     if is_done():
         print(t)
         exit()
     elif t == 100:
         print(101)
-    if t == 53:
-        pdb.set_trace()
+        exit()
+    # if t == 53:
+    #     pdb.set_trace()
 
 print(t)
